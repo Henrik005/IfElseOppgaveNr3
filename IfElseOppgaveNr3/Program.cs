97f8dc1256254f938954badfb3622aaa4704b056
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-11-20 10:17:16 +0100

    Removed unescesary text

--- IfElseOppgaveNr3/Program.cs
+++ IfElseOppgaveNr3/Program.cs
@@ -7,8 +7,6 @@ int num1 = 20;
 int num2 = 10;
 
 
-
-//Doing this one simple and easy because i spent too much time on it because of file problems. :c
 bool NumberCheck()
 {
     if (num1 == 30 || num2 == 30 || (num1 + num2) == 30)
